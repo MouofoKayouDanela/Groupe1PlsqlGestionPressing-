2a40f6d736f1ced407c2f875e791d2cda6a4289f
KengniJohan <kengnijordan9@gmail.com>
2022-12-26 19:55:35 +0100

    Création d'un utilisateur

---
+++ utilisateur.sql
@@ -0,0 +1,30 @@
+SET FEEDBACK 1
+SET NUMWIDTH 10
+SET LINESIZE 80
+SET TRIMSPOOL ON
+SET TAB OFF
+SET PAGESIZE 100
+SET ECHO OFF
+SET VERIFY OFF
+alter session set container=xepdb1;
+DEFINE pass     = op
+DEFINE tbs      = users
+DEFINE ttbs     = temp
+DEFINE pass_sys = op
+DEFINE log_path = logs
+DEFINE connect_string     = //localhost:1521/xepdb1
+DEFINE spool_file = &log_path.OpenPressing.log
+SPOOL &spool_file
+PROMPT*************** Creation utilisateur ***************
+CREATE USER OpenPressing IDENTIFIED BY &pass;
+
+ALTER USER OpenPressing DEFAULT TABLESPACE &tbs
+              QUOTA UNLIMITED ON &tbs;
+
+ALTER USER OpenPressing TEMPORARY TABLESPACE &ttbs;
+
+GRANT CREATE SESSION, CREATE VIEW, ALTER SESSION, CREATE SEQUENCE TO OpenPressing;
+GRANT CREATE SYNONYM, CREATE DATABASE LINK, RESOURCE , UNLIMITED TABLESPACE TO OpenPressing;
+CONNECT sys/&pass_sys@&connect_string AS SYSDBA;
+GRANT execute ON sys.dbms_stats TO OpenPressing;
+CONNECT OpenPressing/&pass@&connect_string
