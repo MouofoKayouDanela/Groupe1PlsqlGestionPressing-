fd2702968d9505e36da1848711f526a2d1a09287
Fossa18 <93674717+fossa18@users.noreply.github.com>
2022-12-27 15:52:49 +0100

    Update Utilisateur.sql

--- Tables/Utilisateur.sql
+++ Tables/Utilisateur.sql
@@ -1,12 +1,12 @@
 CREATE TABLE UTILISATEUR
 (
     id VARCHAR2(30),
-    nom VARCHAR2(255) , 
-    prenom VARCHAR2(255), 
-    genre CHAR(15) ,
-    date_naissance  DATE,
-    email VARCHAR2(255),
-    telephone NUMBER ,
-    nom_utilisateur VARCHAR(255) ,
-    mot_de_passe VARCHAR2(255) 
+    Nom VARCHAR2(255) , 
+    Prenom VARCHAR2(255), 
+    Genre CHAR(15) ,
+    Date_naissance  DATE,
+    Email VARCHAR2(255),
+    Téléphone NUMBER ,
+    Nom_utilisateur VARCHAR(255) ,
+    Mot_de_passe VARCHAR2(255) 
 );
