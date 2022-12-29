0f73c021290eae378eb5d0369615992db0ea38b9
KengniJohan <kengnijordan9@gmail.com>
2022-12-29 23:50:06 +0100

    Mise à jour du fichier CE_Agent.sql

--- Objets/Tables/Contraintes/ClesEtrangeres/CE_Agent.sql
+++ Objets/Tables/Contraintes/ClesEtrangeres/CE_Agent.sql
@@ -1,6 +1,7 @@
 ALTER TABLE AGENT
 ADD
 (
-    CONSTRAINT ce_agence_id_agence FOREIGN KEY (id_agence) REFERENCES Agence(id),
-    CONSTRAINT ce_agence_id_role FOREIGN KEY (id_role) REFERENCES Role(id)
+    CONSTRAINT ce_agent_id FOREIGN KEY (id) REFERENCES UTILISATEUR (id),
+    CONSTRAINT ce_agent_id_agence FOREIGN KEY (id_agence) REFERENCES Agence(id),
+    CONSTRAINT ce_agent_id_role FOREIGN KEY (id_role) REFERENCES Role(id)
 );
