e784345b423c85a116c9de82ee386161ffcabb82
KengniJohan <kengnijordan9@gmail.com>
2022-12-30 22:18:33 +0100

    Création du fichier index de création des contraintes de clé primaire

---
+++ Objets/Tables/Contraintes/ClesPrimaires/Index_Cles_Primaires.sql
@@ -0,0 +1,27 @@
+PROMPT CREATION DES CONTRAINTES DE CLES PRIMAIRES
+@@CP_Administrateur
+@@CP_Agence
+@@CP_Agent
+@@CP_Annonce
+@@CP_Besoin
+@@CP_Categorie_Linge
+@@CP_Categorie_Service
+@@CP_Client
+@@CP_Commande
+@@CP_Details_Besoin
+@@CP_Linge_Agence
+@@CP_Linge
+@@CP_Message
+@@CP_Offre
+@@CP_Pays
+@@CP_Pressing
+@@CP_Promotion
+@@CP_Proprietaire
+@@CP_Quartier
+@@CP_Role
+@@CP_Service_Agence
+@@CP_Service
+@@CP_Type_Linge
+@@CP_Type_Service
+@@CP_Utilisateur
+@@CP_Ville
