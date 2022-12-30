77b23d5dd9a0bf67417165e82242a7e16fe50da7
KengniJohan <kengnijordan9@gmail.com>
2022-12-30 22:23:30 +0100

    Création du fichier Index des clés étrangères

---
+++ Objets/Tables/Contraintes/ClesEtrangeres/Index_Cles_Etrangeres.sql
@@ -0,0 +1,19 @@
+PROMPT CREATION DES CONTRAINTES DE CLES ETRANGERES
+@@CE_Administrateur
+@@CE_Proprietaire
+@@CE_Pressing
+@@CE_Ville
+@@CE_Quartier
+@@CE_Agence
+@@CE_Agent
+@@CE_service
+@@CE_Linge
+@@CE_Service_Agence
+@@CE_Linge_Agence
+@@CE_Offre
+@@CE_Promotion
+@@CE_Client
+@@CE_Besoin
+@@CE_Details_Besoin
+@@CE_Message
+@@CE_Commande
