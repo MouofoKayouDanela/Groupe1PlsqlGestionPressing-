b121e85aeb419bf3f9dc40603613ef7b8acde107
KengniJohan <kengnijordan9@gmail.com>
2022-12-30 22:23:56 +0100

    Création du fichier index de toutes les contraintes

---
+++ Objets/Tables/Contraintes/Index_Contraintes.sql
@@ -0,0 +1,6 @@
+PROMPT CREATION DES DIFFERENTES CONTRAINTES
+@./Objets/Tables/Contraintes/ValeursObligatoires/Index_Valeurs_Obligatoires
+@./Objets/Tables/Contraintes/ValeursUniques/Index_Valeurs_Uniques
+@./Objets/Tables/Contraintes/ValeursDeDomaine/Index_Valeurs_Domaines
+@./Objets/Tables/Contraintes/ClesPrimaires/Index_Cles_Primaires
+@./Objets/Tables/Contraintes/ClesEtrangeres/Index_Cles_Etrangeres
