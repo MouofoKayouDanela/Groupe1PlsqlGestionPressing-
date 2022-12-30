8f9447e0243e6cd6a9e847d1b47bcea21a5ad50e
Ryan Zebaze <bravezebaze@gmail.com>
2022-12-30 13:41:39 +0100

    insertion des tables pays villes et quartier

---
+++ Objets/Donnees/DO.Ville.sql
@@ -0,0 +1,10 @@
+insert into VILLE (id, nom, id_pays) values (VI001, Douala, PA001);
+insert into VILLE (id, nom, id_pays) values (VI002, Yaounde, PA002);
+insert into VILLE (id, nom, id_pays) values (VI003, Bafoussam, PA003);
+insert into VILLE (id, nom, id_pays) values (VI004, Kribi, PA004);
+insert into VILLE (id, nom, id_pays) values (VI005, Douala, PA005);
+insert into VILLE (id, nom, id_pays) values (VI006, Yaounde, PA006);
+insert into VILLE (id, nom, id_pays) values (VI007, Bafoussam, PA007);
+insert into VILLE (id, nom, id_pays) values (VI008, Kribi, PA008);
+insert into VILLE (id, nom, id_pays) values (VI009, Douala, PA009);
+insert into VILLE (id, nom, id_pays) values (VI010, Yaounde, PA010);
