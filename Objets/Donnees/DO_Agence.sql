3350f6e9f31f992d81925b88c012bd3330df5bb7
Nikiledji Aurore <auroranikiledji@gmail.com>
2023-01-02 22:12:19 +0100

    Autres insertions

--- Objets/Donnees/DO_Agence.sql
+++ Objets/Donnees/DO_Agence.sql
@@ -2,13 +2,69 @@ INSERT INTO
 AGENCE (id, id_pressing, id_quartier)
 VALUES (
     'AG001',
-    '',
-    ''
+    'PR001',
+    'QU002'
 );
 INSERT INTO 
 AGENCE (id, id_pressing, id_quartier)
 VALUES (
     'AG002',
-    '',
-    ''
+    'PR001',
+    'QU008'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG003',
+    'PR002',
+    'QU008'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG004',
+    'PR002',
+    'QU004'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG005',
+    'PR003',
+    'QU001'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG006',
+    'PR003',
+    'QU004'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG007',
+    'PR003',
+    'QU009'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG008',
+    'PR003',
+    'QU007'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG009',
+    'PR001',
+    'QU010'
+);
+INSERT INTO 
+AGENCE (id, id_pressing, id_quartier)
+VALUES (
+    'AG010',
+    'PR002',
+    'QU006'
 );
--- Objets/Donnees/DO_Pressing.sql
+++ Objets/Donnees/DO_Pressing.sql
@@ -2,62 +2,21 @@ INSERT INTO
 PRESSING (id, nom, id_proprietaire)
 VALUES (
     'PR001',
-    'Elegance pressing',
-    ''
+    'Bling pressing',
+    'UT002'
 );
 INSERT INTO 
 PRESSING (id, nom, id_proprietaire)
 VALUES (
     'PR002',
-    'Bling pressing',
-    ''
+    'Speed clean services',
+    'UT003'
 );
 INSERT INTO 
 PRESSING (id, nom, id_proprietaire)
 VALUES (
     'PR003',
-    'Pressing cote azur',
-    ''
-);
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR005',
-    'Majestic pressing',
-    ''
-);
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR006',
-    'Royal pressing',
-    ''
-);
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR007',
-    'Universal pressing',
-    ''
-);
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR008',
-    'Pressing du boulevard',
-    ''
-);
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR009',
     'Elegance pressing',
-    ''
+    'UT004'
 );
-INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
-VALUES (
-    'PR010',
-    'Speed clean service',
-    ''
-);
+
