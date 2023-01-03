3153e1d12303daa67cba44639617e08e823bfae2
KengniJohan <kengnijordan9@gmail.com>
2023-01-03 22:55:27 +0100

    Ajout des données convenablement  l'attribut "date_embauche"

--- Objets/Donnees/DO_Agent.sql
+++ Objets/Donnees/DO_Agent.sql
@@ -1,236 +1,236 @@
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT001',
     'AG001',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT002',
     'AG001',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT003',
     'AG001',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT004',
     'AG001',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT005',
     'AG002',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT006',
     'AG002',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT007',
     'AG002',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT008',
     'AG002',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT009',
     'AG003',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT010',
     'AG003',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT011',
     'AG003',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT012',
     'AG004',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT013',
     'AG004',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT014',
     'AG004',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT015',
     'AG004',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT016',
     'AG004',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT017',
     'AG006',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT018',
     'AG006',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT019',
     'AG006',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT020',
     'AG006',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT021',
     'AG008',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT022',
     'AG008',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT023',
     'AG008',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT024',
     'AG008',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT025',
     'AG009',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT026',
     'AG009',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT027',
     'AG009',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT028',
     'AG009',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT029',
     'AG009',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT030',
     'AG005',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT031',
     'AG005',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT032',
     'AG005',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT033',
     'AG005',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT034',
     'AG005',
@@ -244,35 +244,35 @@ VALUES (
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT036',
     'AG007',
     'RO002'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT037',
     'AG007',
     'RO004'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT038',
     'AG007',
     'RO003'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT039',
     'AG010',
     'RO001'
 );
 INSERT INTO 
-AGENT (id, id_agence, id_role)
+AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT040',
     'AG010',
