7df4b7843f35e08188674a50e0e223721c92b9be
KengniJohan <kengnijordan9@gmail.com>
2023-01-03 22:59:45 +0100

    Ajout de l'exécution des fichiers VDD_Agence.sql, VDD_Agent.sql, VDD_Client.sql, VDD_Pressing.sql et VDD_Proprietaire.sql

--- Objets/Tables/Contraintes/ValeursDeDomaine/Index_Valeurs_Domaines.sql
+++ Objets/Tables/Contraintes/ValeursDeDomaine/Index_Valeurs_Domaines.sql
@@ -1,4 +1,9 @@
 PROMPT CREATION DES CONTRAINTES DES VALEURS DE DOMAINE
+@@VDD_Agent
+@@VDD_Agence
+@@VDD_Client
 @@VDD_Details_Besoin
 @@VDD_Linge_agence
+@@VDD_Pressing
+@@VDD_Proprietaire
 @@VDD_Utilisateur
