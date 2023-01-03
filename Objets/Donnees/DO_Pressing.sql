c87575d8fbaa1f6d2308386fd98d05078c1e1c82
KengniJohan <kengnijordan9@gmail.com>
2023-01-03 22:49:06 +0100

    Ajout des données considérant l'attribut "date_creation"

--- Objets/Donnees/DO_Pressing.sql
+++ Objets/Donnees/DO_Pressing.sql
@@ -1,50 +1,57 @@
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR001',
     'Bling pressing',
+    TO_DATE("17/01/2019", "dd/mm/yyyy"),
     'UT002'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR002',
     'Speed clean services',
+    TO_DATE("01/12/2019", "dd/mm/yyyy"),
     'UT003'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR003',
     'Elegance pressing',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'UT004'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR004',
     'DG pressing',
+    TO_DATE("25/10/2019", "dd/mm/yyyy"),
     'UT002'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR005',
     'Universal pressing',
-    'UT001'
+    TO_DATE("22/03/2021", "dd/mm/yyyy"),
+    'UT002'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR006',
     'Pressing cote azur',
+    TO_DATE("22/03/2021", "dd/mm/yyyy"),
     'UT003'
 );
 INSERT INTO 
-PRESSING (id, nom, id_proprietaire)
+PRESSING (id, nom, date_creation, id_proprietaire)
 VALUES (
     'PR007',
     'Glorious pressing',
+    TO_DATE("13/09/2022", "dd/mm/yyyy"),
     'UT003'
 );
 
