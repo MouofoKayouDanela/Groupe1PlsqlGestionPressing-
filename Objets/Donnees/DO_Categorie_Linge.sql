d73a148baf27a606473d0110c9ac4ea06f269339
Jordan Tchantchou <Jordantchantchou@gmail.com>
2023-01-03 16:19:31 +0100

    Insertion des tables Type_Linge et Categorie_Linge

---
+++ Objets/Donnees/DO_Categorie_Linge.sql
@@ -0,0 +1,100 @@
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL001',
+    'Coton'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL002',
+    'Lin'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL003',
+    'Laine'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL004',
+    'Soie'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL005',
+    'Viscose'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL006',
+    'Acetale'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL007',
+    'Polyamide'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL008',
+    'Polyacrylique'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL009',
+    'Polyester'
+);
+INSERT INTO CATEGORIE_LINGE
+(
+    id,
+    Nom
+)
+VALUES
+(
+    'CL010',
+    'Elasthanne'
+);
---
+++ Objets/Donnees/DO_Type_Linge.sql
@@ -0,0 +1,260 @@
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL001',
+    'Chemises'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL002',
+    'Tricot'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL003',
+    'Pantalon'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL004',
+    'Culotte'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL005',
+    'BouBou'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL006',
+    'Robe'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL007',
+    'Jupe'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL008',
+    'Chemisette'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL009',
+    'Gang'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL010',
+    'Tapis'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL011',
+    'Rideau'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL012',
+    'Serviette'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL013',
+    'Penoir'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL014',
+    'Chaussure'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL015',
+    'Revetement Canape'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL016',
+    'Cabas'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL017',
+    'Nappe'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL018',
+    'Chaussettes'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL019',
+    'Veste'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL020',
+    'Vestons'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL021',
+    'Manteau'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL022',
+    'Trench'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL023',
+    'Matelas'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL024',
+    'Housse'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL025',
+    'Oreillers'
+);
+INSERT INTO TYPE_LINGE
+(
+    id,
+    intitule
+)
+VALUES
+(
+    'TL026',
+    'Valise'
+);
