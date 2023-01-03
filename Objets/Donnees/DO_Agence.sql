f44ea85c452dcabedbf248154390f185d150aa3f
KengniJohan <kengnijordan9@gmail.com>
2023-01-03 22:58:25 +0100

    Ajout des données suivant l'attribut "date_creation"

--- Objets/Donnees/DO_Agence.sql
+++ Objets/Donnees/DO_Agence.sql
@@ -1,70 +1,80 @@
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG001',
+    TO_DATE("17/01/2019", "dd/mm/yyyy"),
     'PR001',
     'QU002'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG002',
+    TO_DATE("09/11/2019", "dd/mm/yyyy"),
     'PR001',
     'QU008'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG003',
+    TO_DATE("01/12/2019", "dd/mm/yyyy"),
     'PR002',
     'QU008'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG004',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'PR002',
     'QU004'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG005',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'PR003',
     'QU001'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG006',
+    TO_DATE("31/08/2020", "dd/mm/yyyy"),
     'PR003',
     'QU004'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG007',
+    TO_DATE("23/03/2021", "dd/mm/yyyy"),
     'PR005',
     'QU009'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG008',
+    TO_DATE("03/02/2021", "dd/mm/yyyy"),
     'PR003',
     'QU007'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG009',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'PR001',
     'QU010'
 );
 INSERT INTO 
-AGENCE (id, id_pressing, id_quartier)
+AGENCE (id, date_creation, id_pressing, id_quartier)
 VALUES (
     'AG010',
+    TO_DATE("13/09/2022", "dd/mm/yyyy"),
     'PR007',
     'QU006'
 );
