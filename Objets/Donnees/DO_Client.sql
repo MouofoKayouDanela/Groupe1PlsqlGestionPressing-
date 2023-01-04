cacdbbd8515612a4bd959d2e8a087652bddbc559
KengniJohan <kengnijordan9@gmail.com>
2023-01-04 08:43:13 +0100

    Ajout des attributs suivant l'attribut "date_creation"

--- Objets/Donnees/DO_Client.sql
+++ Objets/Donnees/DO_Client.sql
@@ -1,48 +1,60 @@
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT010'
+    'UT010',
+    TO_DATE("25/02/2019", "dd/mm/yyyy")
 );
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT011'
+    'UT011',
+    TO_DATE("25/02/2019", "dd/mm/yyyy")
 );
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT012'
+    'UT012',
+    TO_DATE("30/05/2019", "dd/mm/yyyy")
 );
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT013'
+    'UT013',
+    TO_DATE("31/05/2019", "dd/mm/yyyy")
 );
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT014'
+    'UT014',
+    TO_DATE("13/07/2019", "dd/mm/yyyy")
 );
 INSERT INTO CLIENT
 (
-    id 
+    id,
+    date_inscription 
 )
 VALUES
 (
-    'UT015'
+    'UT015',
+    TO_DATE("19/09/2020", "dd/mm/yyyy")
 );
