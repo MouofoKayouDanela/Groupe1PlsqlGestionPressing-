67169c11c008cb70a81901aa849dc4294f9763bd
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2023-01-04 10:28:15 +0100

    modification des insertions de la table linge_agence et des valeurs obligatoires de linge_agence et service_agence

--- Objets/Donnees/DO_Linge_Agence.sql
+++ Objets/Donnees/DO_Linge_Agence.sql
@@ -1,445 +1,426 @@
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait  
 )
 VALUES
 (
     'CL001',
     'AG001',
-    TO_DATE('03/01/2023', 'dd/mm/yyyy'),
+    TO_DATE('17/01/2019', 'dd/mm/yyyy'),
     'TL001',
-    TO_DATE('10/01/2023', 'dd/mm/yyyy')
+    TO_DATE('20/01/2019', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+     id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait  
 )
 VALUES
 (
-     'CL002',
-     'AG002',
-     TO_DATE('12/01/2023', 'dd/mm/yyyy'),
+    'CL002',
+    'AG002',
+    TO_DATE('09/11/2019', 'dd/mm/yyyy'),
     'TL002',
-    TO_DATE('15/01/2023', 'dd/mm/yyyy')
+    TO_DATE('15/11/2019', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+     id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait  
 )
 VALUES
 (
     'CL003',
     'AG003',
-    TO_DATE('11/02/2023', 'dd/mm/yyyy'),
+    TO_DATE('01/12/2019', 'dd/mm/yyyy'),
     'TL003',
-    TO_DATE('14/02/2023', 'dd/mm/yyyy')
+    TO_DATE('01/12/2019', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait   
 )
 VALUES
 ( 
     'CL004',
     'AG004',
-    TO_DATE('23/02/2023', 'dd/mm/yyyy'),
+     TO_DATE('28/04/2020', 'dd/mm/yyyy'),
     'TL004',
-    TO_DATE('28/03/2023', 'dd/mm/yyyy')
+     TO_DATE('30/04/2020', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+   id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait   
 )
 VALUES
 ( 
     'CL005',
     'AG005',
-    TO_DATE('08/03/2023', 'dd/mm/yyyy'),
+     TO_DATE('30/04/2020', 'dd/mm/yyyy'),
     'TL005',
-    TO_DATE('15/03/2023', 'dd/mm/yyyy')
+    TO_DATE('05/05/2020', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+     id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait  
 )
 VALUES
 (  
     'CL006',
     'AG006',
-    TO_DATE('10/03/2023', 'dd/mm/yyyy'),
+     TO_DATE('31/08/2020', 'dd/mm/yyyy'),
     'TL006',
-    TO_DATE('13/03/2023', 'dd/mm/yyyy')
+    TO_DATE('10/09/2020', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+     id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait   
 )
 VALUES
 (  
     'CL007',
     'AG007',
-    TO_DATE('25/03/2023', 'dd/mm/yyyy'),
+    TO_DATE('23/03/2021', 'dd/mm/yyyy'),
     'TL007',
-    TO_DATE('31/03/2023', 'dd/mm/yyyy')
+   TO_DATE('29/03/2021', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+     id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait   
 )
 VALUES
 (  
     'CL008',
     'AG008',
-    TO_DATE('03/04/2023', 'dd/mm/yyyy'),
+    TO_DATE('03/02/2021', 'dd/mm/yyyy'),
     'TL008',
-    TO_DATE('08/04/2023', 'dd/mm/yyyy')
+    TO_DATE('10/02/2021', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait   
 )
 VALUES
 (  
     'CL009',
     'AG009',
-    TO_DATE('11/04/2023', 'dd/mm/yyyy'),
+    TO_DATE('28/05/2022', 'dd/mm/yyyy'),
     'TL009',
-    TO_DATE('16/04/2023', 'dd/mm/yyyy')
+    TO_DATE('30/05/2022', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type ,
+    Date_retrait  
 )
 VALUES
 (  
     'CL010',
     'AG010',
-    TO_DATE('22/04/2023', 'dd/mm/yyyy'),
+    TO_DATE('15/09/2022', 'dd/mm/yyyy'),
     'TL010',
-    TO_DATE('28/04/2023', 'dd/mm/yyyy')
+    TO_DATE('20/09/2022', 'dd/mm/yyyy')
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+   id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type 
 )
 VALUES
 (  
     'CL011',
-    TO_DATE('18/04/2023', 'dd/mm/yyyy'),
-    'TL011',
-    TO_DATE('26/04/2023', 'dd/mm/yyyy')
+    'AG009',
+    TO_DATE('30/05/2022', 'dd/mm/yyyy'),
+    'TL011'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+  id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL012',
-    TO_DATE('01/05/2023', 'dd/mm/yyyy'),
-    'TL012',
-    TO_DATE('05/05/2023', 'dd/mm/yyyy')
+    'AG007',
+    TO_DATE('25/03/2021', 'dd/mm/yyyy'),
+    'TL012'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL013',
-    TO_DATE('10/05/2023', 'dd/mm/yyyy'),
-    'TL013',
-    TO_DATE('15/05/2023', 'dd/mm/yyyy')
+    'AG006',
+    TO_DATE('02/09/2020', 'dd/mm/yyyy'),
+    'TL013'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL014',
-    TO_DATE('22/05/2023', 'dd/mm/yyyy'),
-    'TL014',
-    TO_DATE('28/05/2023', 'dd/mm/yyyy')
+    'AG001',
+    TO_DATE('19/01/2019', 'dd/mm/yyyy'),
+    'TL014'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL015',
-     TO_DATE('07/06/2023', 'dd/mm/yyyy'),
-    'TL015',
-    TO_DATE('10/06/2023', 'dd/mm/yyyy')
+    'AG003',
+    TO_DATE('01/12/2019', 'dd/mm/yyyy'),
+    'TL015'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL016',
-    TO_DATE('14/06/2023', 'dd/mm/yyyy'),
-    'TL016',
-    TO_DATE('18/06/2023', 'dd/mm/yyyy')
+    'AG002',
+    TO_DATE('11/11/2019', 'dd/mm/yyyy'),
+    'TL016'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL017',
-    TO_DATE('20/06/2023', 'dd/mm/yyyy'),
-    'TL017',
-    TO_DATE('25/06/2023', 'dd/mm/yyyy')
+    'AG010',
+    TO_DATE('19/09/2022', 'dd/mm/yyyy'),
+    'TL017'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL018',
-    TO_DATE('04/06/2023', 'dd/mm/yyyy'),
-    'TL018',
-    TO_DATE('08/06/2023', 'dd/mm/yyyy')
+    'AG007',
+    TO_DATE('25/03/2021', 'dd/mm/yyyy'),
+    'TL018'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL019',
-    TO_DATE('07/07/2023', 'dd/mm/yyyy'),
-    'TL019',
-    TO_DATE('12/07/2023', 'dd/mm/yyyy')
+    'AG007',
+    TO_DATE('24/03/2021', 'dd/mm/yyyy'),
+    'TL019'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+   id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL020',
-    TO_DATE('04/07/2023', 'dd/mm/yyyy'),
-    'TL020',
-    TO_DATE('09/07/2023', 'dd/mm/yyyy')
+    'AG006',
+    TO_DATE('01/09/2020', 'dd/mm/yyyy'),
+    'TL020'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+   id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL021',
-    TO_DATE('12/07/2023', 'dd/mm/yyyy'),
-    'TL021',
-    TO_DATE('18/07/2023', 'dd/mm/yyyy')
+    'AG004',
+    TO_DATE('27/04/2020', 'dd/mm/yyyy'),
+    'TL021'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+   id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL022',
-    TO_DATE('20/07/2023', 'dd/mm/yyyy'),
-    'TL022',
-    TO_DATE('25/07/2023', 'dd/mm/yyyy')
+    'AG002',
+    TO_DATE('12/11/2019', 'dd/mm/yyyy'),
+    'TL022'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL023',
-    TO_DATE('03/08/2023', 'dd/mm/yyyy'),
-    'TL023',
-    TO_DATE('05/08/2023', 'dd/mm/yyyy')
+    'AG003',
+    TO_DATE('03/12/2019', 'dd/mm/yyyy'),
+    'TL023'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type
 )
 VALUES
 (  
     'CL024',
-    TO_DATE('08/08/2023', 'dd/mm/yyyy'),
-    'TL024',
-    TO_DATE('13/08/2023', 'dd/mm/yyyy')
+    'AG005',
+    TO_DATE('02/05/2020', 'dd/mm/yyyy'),
+    'TL024'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type 
 )
 VALUES
 (  
     'CL025',
-    TO_DATE('15/08/2023', 'dd/mm/yyyy'),
-    'TL025',
-    TO_DATE('20/08/2023', 'dd/mm/yyyy')
+    'AG005',
+    TO_DATE('05/05/2020', 'dd/mm/yyyy'),
+    'TL025'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout,
+    id_type 
 )
 VALUES
 (  
     'CL026',
-    TO_DATE('10/09/2023', 'dd/mm/yyyy'),
-    'TL026',
-    TO_DATE('14/09/2023', 'dd/mm/yyyy')
+    'AG007',
+    TO_DATE('24/04/2021', 'dd/mm/yyyy'),
+    'TL026'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type 
 )
 VALUES
 (  
     'CL027',
-    TO_DATE('05/09/2023', 'dd/mm/yyyy'),
-    'TL027',
-    TO_DATE('11/09/2023', 'dd/mm/yyyy')
+   'AG009',
+    TO_DATE('28/05/2022', 'dd/mm/yyyy'),
+    'TL027'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie ,
+    id_agence ,
+    Date_ajout ,
+    id_type 
 )
 VALUES
 (  
     'CL028',
-    TO_DATE('16/09/2023', 'dd/mm/yyyy'),
-    'TL028',
-    TO_DATE('20/09/2023', 'dd/mm/yyyy')
+    'AG010',
+    TO_DATE('15/09/2022', 'dd/mm/yyyy'),
+    'TL028'
 );
 INSERT INTO LINGE_AGENCE
 (
-    id_categorie VARCHAR2(50),
-    id_agence VARCHAR2(50),
-    Date_ajout DATE,
-    id_type VARCHAR2(50),
-    Date_retrait DATE  
+    id_categorie,
+    id_agence ,
+    Date_ajout ,
+    id_type
 )
 VALUES
 (  
     'CL029',
-    TO_DATE('25/09/2023', 'dd/mm/yyyy'),
-    'TL029',
-    TO_DATE('30/09/2023', 'dd/mm/yyyy')
+    'AG004',
+    TO_DATE('06/05/2020', 'dd/mm/yyyy'),
+    'TL029'
 );
