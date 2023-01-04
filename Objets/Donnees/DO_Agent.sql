ac16969d0e201579e6b58a1477ac50b5fe14444c
KengniJohan <kengnijordan9@gmail.com>
2023-01-04 08:30:47 +0100

    Modification des  données suivant l'attribut "date_creation"

--- Objets/Donnees/DO_Agent.sql
+++ Objets/Donnees/DO_Agent.sql
@@ -2,6 +2,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT001',
+    TO_DATE("17/01/2019", "dd/mm/yyyy"),
     'AG001',
     'RO001'
 );
@@ -9,6 +10,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT002',
+    TO_DATE("18/01/2019", "dd/mm/yyyy"),
     'AG001',
     'RO003'
 );
@@ -16,6 +18,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT003',
+    TO_DATE("18/01/2019", "dd/mm/yyyy"),
     'AG001',
     'RO004'
 );
@@ -23,6 +26,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT004',
+    TO_DATE("18/01/2019", "dd/mm/yyyy"),
     'AG001',
     'RO002'
 );
@@ -30,6 +34,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT005',
+    TO_DATE("09/11/2019", "dd/mm/yyyy"),
     'AG002',
     'RO003'
 );
@@ -37,6 +42,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT006',
+    TO_DATE("09/11/2019", "dd/mm/yyyy"),
     'AG002',
     'RO001'
 );
@@ -44,6 +50,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT007',
+    TO_DATE("09/11/2019", "dd/mm/yyyy"),
     'AG002',
     'RO002'
 );
@@ -51,6 +58,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT008',
+    TO_DATE("09/11/2019", "dd/mm/yyyy"),
     'AG002',
     'RO001'
 );
@@ -58,6 +66,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT009',
+    TO_DATE("01/12/2019", "dd/mm/yyyy"),
     'AG003',
     'RO002'
 );
@@ -65,6 +74,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT010',
+    TO_DATE("01/12/2019", "dd/mm/yyyy"),
     'AG003',
     'RO003'
 );
@@ -72,6 +82,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT011',
+    TO_DATE("01/12/2019", "dd/mm/yyyy"),
     'AG003',
     'RO001'
 );
@@ -79,6 +90,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT012',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'AG004',
     'RO003'
 );
@@ -86,6 +98,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT013',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'AG004',
     'RO003'
 );
@@ -93,6 +106,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT014',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'AG004',
     'RO001'
 );
@@ -100,6 +114,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT015',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'AG004',
     'RO002'
 );
@@ -107,34 +122,39 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT016',
+    TO_DATE("26/04/2020", "dd/mm/yyyy"),
     'AG004',
     'RO002'
 );
 INSERT INTO 
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
-    'AT017',
+    'AT017'
+    TO_DATE("31/08/2020", "dd/mm/yyyy"),
     'AG006',
     'RO001'
 );
 INSERT INTO 
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
-    'AT018',
+    'AT018'
+    TO_DATE("31/08/2020", "dd/mm/yyyy"),
     'AG006',
     'RO003'
 );
 INSERT INTO 
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
-    'AT019',
+    'AT019'
+    TO_DATE("31/08/2020", "dd/mm/yyyy"),
     'AG006',
     'RO002'
 );
 INSERT INTO 
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
-    'AT020',
+    'AT020'
+    TO_DATE("31/08/2020", "dd/mm/yyyy"),
     'AG006',
     'RO004'
 );
@@ -142,6 +162,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT021',
+    TO_DATE("03/02/2021", "dd/mm/yyyy"),
     'AG008',
     'RO002'
 );
@@ -149,6 +170,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT022',
+    TO_DATE("03/02/2021", "dd/mm/yyyy"),
     'AG008',
     'RO003'
 );
@@ -156,6 +178,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT023',
+    TO_DATE("03/02/2021", "dd/mm/yyyy"),
     'AG008',
     'RO004'
 );
@@ -163,6 +186,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT024',
+    TO_DATE("03/02/2021", "dd/mm/yyyy"),
     'AG008',
     'RO001'
 );
@@ -170,6 +194,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT025',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'AG009',
     'RO002'
 );
@@ -177,6 +202,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT026',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'AG009',
     'RO003'
 );
@@ -184,6 +210,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT027',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'AG009',
     'RO004'
 );
@@ -191,6 +218,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT028',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'AG009',
     'RO001'
 );
@@ -198,6 +226,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT029',
+    TO_DATE("24/05/2022", "dd/mm/yyyy"),
     'AG009',
     'RO002'
 );
@@ -205,6 +234,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT030',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'AG005',
     'RO003'
 );
@@ -212,6 +242,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT031',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'AG005',
     'RO004'
 );
@@ -219,6 +250,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT032',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'AG005',
     'RO002'
 );
@@ -226,6 +258,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT033',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'AG005',
     'RO001'
 );
@@ -233,6 +266,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT034',
+    TO_DATE("30/04/2020", "dd/mm/yyyy"),
     'AG005',
     'RO003'
 );
@@ -240,6 +274,7 @@ INSERT INTO
 AGENT (id, id_agence, id_role)
 VALUES (
     'AT035',
+    TO_DATE("23/03/2021", "dd/mm/yyyy"),
     'AG007',
     'RO001'
 );
@@ -247,6 +282,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT036',
+    TO_DATE("23/03/2021", "dd/mm/yyyy"),
     'AG007',
     'RO002'
 );
@@ -254,6 +290,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT037',
+    TO_DATE("23/03/2021", "dd/mm/yyyy"),
     'AG007',
     'RO004'
 );
@@ -261,6 +298,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT038',
+    TO_DATE("23/03/2021", "dd/mm/yyyy"),
     'AG007',
     'RO003'
 );
@@ -268,6 +306,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT039',
+    TO_DATE("13/09/2022", "dd/mm/yyyy"),
     'AG010',
     'RO001'
 );
@@ -275,6 +314,7 @@ INSERT INTO
 AGENT (id, date_embauche, id_agence, id_role)
 VALUES (
     'AT040',
+    TO_DATE("13/09/2022", "dd/mm/yyyy"),
     'AG010',
     'RO004'
 );
