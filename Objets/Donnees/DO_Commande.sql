06321bcba5930f0b863fc4869029fa302f74715f
Jordan Tchantchou <Jordantchantchou@gmail.com>
2023-01-06 12:10:54 +0100

    Insertion de la table commande

---
+++ Objets/Donnees/DO_Commande.sql
@@ -0,0 +1,132 @@
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C001',
+    'Lavage a sec',
+    '675800345',
+    'QU001',
+    'QU001',
+    'BE001',
+    'AG001',
+    TO_DATE('28/02/2019', 'dd/mm/yyyy')
+);
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C002',
+    'Lavage a eau',
+    '680457900',
+    'QU002',
+    'QU002',
+    'BE002',
+    'AG002',
+    TO_DATE('03/03/2019', 'dd/mm/yyyy')
+);
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C003',
+    'Lavage simple',
+    '654637896',
+    'QU003',
+    'QU003',
+    'BE003',
+    'AG003',
+    TO_DATE('05/06/2019', 'dd/mm/yyyy')
+);
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C004',
+    'Lavage a froid',
+    '699465220',
+    'QU004',
+    'QU004',
+    'BE004',
+    'AG004',
+    TO_DATE('05/06/2019', 'dd/mm/yyyy')
+);
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C005',
+    'Lavage a induction',
+    '662066781',
+    'QU005',
+    'QU005',
+    'BE005',
+    'AG005',
+    TO_DATE('30/07/2019', 'dd/mm/yyyy')
+);
+INSERT INTO COMMANDE
+(
+    id,
+    description,
+    adresse_Payement,
+    adresse_Ramassage,
+    adresse_Livraison,
+    id_besoin,
+    id_agence,
+    date_Emission_Message
+)
+VALUES
+(
+    'C006',
+    'Lavage a sec',
+    '652345890',
+    'QU006',
+    'QU006',
+    'BE006',
+    'AG006',
+    TO_DATE('28/09/2019', 'dd/mm/yyyy')
+);
