3af387ebc6dd2f92ee59e766f29fc8988d79c128
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2023-01-09 22:08:04 +0100

    modification de la fonctionnalites se connecter du proprietaire

--- Objets/Fonctions/FO_Consulter_Status.sql
+++ Objets/Fonctions/FO_Consulter_Status.sql
@@ -1,23 +1,25 @@
 CREATE OR REPLACE FUNCTION Cons_Status(username UTILISATEUR.Nom_utilisateur%TYPE, password UTILISATEUR.mot_de_passe%TYPE)
-RETURN int IS 
+RETURN VARCHAR2 IS 
 CURSOR val IS
-SELECT status 
+SELECT  Statut 
 FROM UTILISATEUR Util
 JOIN  PROPRIETAIRE Pro
 ON (Util.id=Pro.id)
 WHERE Util.Nom_utilisateur =username
 AND 
 Util.Mot_de_passe=password;
-rest INT;
+rest VARCHAR2 (255);
 BEGIN
-OPEN val
-FETCH val 
+OPEN val;
+FETCH val
 INTO rest;
+CLOSE val;
 RETURN rest;
 END;
 /
-
 DECLARE
-
-result int := Cons_Status('&username','&mot_de_passe');
- 
+result VARCHAR2 (255) := Cons_Status('&username','&mot_de_passe');
+BEGIN
+DBMS_OUTPUT.PUT_LINE(result);
+END;
+/
