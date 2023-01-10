36532a7b9cea22317a65fbfa998dc66a2c44d380
Nikiledji Aurore <auroranikiledji@gmail.com>
2023-01-10 20:07:14 +0100

    Modification

--- Objets/Procedure/PO_Ajout_Pressing.sql
+++ Objets/Procedure/PO_Ajout_Pressing.sql
@@ -1,11 +1,10 @@
 SET SERVEROUTPUT ON;
 CREATE OR REPLACE PROCEDURE Ajout_pressing IS 
-DECLARE
 V_ID_PRES PRESSING.id%TYPE;
 V_Sysdate_PRES PRESSING.date_creation%TYPE;
 BEGIN
-V_ID_PRES = 'PR0'
-V_Sysdate_PRES = SYSDATE;
+V_ID_PRES := 'PR0'
+V_Sysdate_PRES := SYSDATE;
 INSERT INTO PRESSING (id,nom,date_creation,id_proprietaire)
         VALUES (
                 V_ID_PRES||seq_pressing.nexval,
--- Objets/Procedure/PO_Ajout_Proprietaire.sql
+++ Objets/Procedure/PO_Ajout_Proprietaire.sql
@@ -1,11 +1,10 @@
 SET SERVEROUTPUT ON;
 CREATE OR REPLACE PROCEDURE Ajout_proprietaire IS 
-DECLARE
 V_ID_PROP PROPRIETAIRE.id%TYPE;
 V_Sysdate_PROP PROPRIETAIRE.date_enregistrement%TYPE;
 BEGIN
-V_ID_PROP = 'UT0';
-V_Sysdate_PROP = SYSDATE;
+V_ID_PROP := 'UT0';
+V_Sysdate_PROP := SYSDATE;
 INSERT ALL
 INTO UTILISATEUR (id,Nom,Prenom,Genre,Date_naissance,Email,Telephone,Nom_utilisateur,Mot_de_passe)
         VALUES (
