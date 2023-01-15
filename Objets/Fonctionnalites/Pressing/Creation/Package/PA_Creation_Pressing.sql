f207e023b9ecd63f85c9fb1ab81d29c3ba9bf1aa
Nikiledji Aurore <auroranikiledji@gmail.com>
2023-01-15 12:59:23 +0100

    PA_Pressing

--- Objets/Fonctionnalites/Pressing/Creation/Package/PA_Creation_Pressing.sql
+++ Objets/Fonctionnalites/Pressing/Creation/Package/PA_Creation_Pressing.sql
@@ -25,15 +25,35 @@ CREATE OR REPLACE PACKAGE BODY PA_PRESSING AS
             DateCreation_PRESS, 
             IdProprietaire_PRESS);
     END Add_pressing; 
-    FUNCTION Verif_nom_pres 
-    (NomPressing nom.PRESSING%TYPE) RETURN VARCHAR2
+    FUNCTION Verif_nom_pressing 
+    (NomPressing PRESSING.nom%TYPE) RETURN VARCHAR2
     IS
     BEGIN
-    END Verif_nom_pres;   
+    Curseur VARCHAR2(255);
+        CURSOR Curseur IS
+            SELECT nom
+            FROM PRESSING
+            WHERE (Pr.Nom=NomPressing);
+        Valeur VARCHAR2 (255);
+        BEGIN
+            OPEN Curseur;
+            FETCH Curseur
+            INTO Valeur;
+                IF Curseur%NOTFOUND THEN
+                    DBMS_OUTPUT.PUT_LINE('Nom de pressing valide');
+                ELSE
+                    DBMS_OUTPUT.PUT_LINE('Desole un pressing exixte deja sous ce nom');
+                END IF;
+            CLOSE Curseur;
+        RETURN Valeur;
+    END Verif_nom_pressing;   
 END PA_PRESSING;
-/  
+/ 
+DECLARE
+    Rendu VARCHAR2 (255) := VERIFY('&NomPressing');
 BEGIN
-    A_PRESSING.Add_pressing("PR020",&NomDePressing,SYSDATE,"UT005");
+    PA_PRESSING.Add_pressing("PR020",&NomDePressing,SYSDATE,"UT005");
+    PA_PRESSING.Verif_nom_pressing(Rendu);
 END;
 /        
 
