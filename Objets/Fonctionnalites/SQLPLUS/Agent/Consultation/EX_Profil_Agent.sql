eef8d15216a17f2487025eafa5f10434cb776b80
KengniJohan <kengnijordan9@gmail.com>
2023-01-26 23:41:44 +0100

    Ajout du paramètre d'exécution id_pressing et finission de la fonctionnalité de navigation entre fonctionnalités

--- Objets/Fonctionnalites/SQLPLUS/Agent/Consultation/EX_Profil_Agent.sql
+++ Objets/Fonctionnalites/SQLPLUS/Agent/Consultation/EX_Profil_Agent.sql
@@ -1,2 +1,13 @@
 ACCEPT nom_utili CHAR PROMPT 'Veuillez entrer le nom de l''agent dont vous voulez consulter le profil : '
-EXECUTE PA_CONSULTATION_AGENT.PO_PROFIL_AGENT('&nom_utili', '&nom_quartier_agence')
+EXECUTE PA_CONSULTATION_AGENT.PO_PROFIL_AGENT('&nom_utili', '&nom_quartier_agence', '&id_pressing')
+PROMPT          A - SORTIR
+ACCEPT mon_choix CHAR PROMPT 'Veuillez entrer le caractere correspondant a votre choix : '
+SET TERM OFF 
+COLUMN script NEW_VALUE v_script 
+SELECT CASE '&mon_choix'
+            WHEN 'A' THEN 'Objets/Fonctionnalites/SQLPLUS/Agent/Consultation/EX_Liste_Agents'
+            ELSE 'Objets/Fonctionnalites/SQLPLUS/Agent/Consultation/ERR_Choix'
+        END AS script
+FROM    DUAL;
+SET TERM ON 
+@&v_script 
