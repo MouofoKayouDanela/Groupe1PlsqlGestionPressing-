98d8f65ab2f437230fa62e61b5ffe7ce448a3c16
KengniJohan <kengnijordan9@gmail.com>
2023-01-26 16:21:18 +0100

    Modification de l'orthographe

--- Objets/Fonctionnalites/SQLPLUS/Agence/Consultation/ERR_Choix.sql
+++ Objets/Fonctionnalites/SQLPLUS/Agence/Consultation/ERR_Choix.sql
@@ -1,7 +1,7 @@
 PROMPT                            CHOIX INCORRECT
 PROMPT Vous devez operer un choix se trouvant dans la liste qui vous a ete propose !
 PROMPT                               A - retour
-ACCEPT mon_choix CHAR PROMPT 'Veuillez entrer le caractere correspondant z votre choix : '
+ACCEPT mon_choix CHAR PROMPT 'Veuillez entrer le caractere correspondant a votre choix : '
 SET TERM OFF --Blockage de l'affichage du resultat de l'instruction select
 SET SERVEROUTPUT ON
 COLUMN script NEW_VALUE v_script --Conservation de la valeur de la variable script dans la variable v_script
