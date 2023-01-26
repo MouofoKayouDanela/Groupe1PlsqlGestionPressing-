e54ac25759d609ee58f4dad0dd58f9d377f06552
Nikiledji Aurore <auroranikiledji@gmail.com>
2023-01-26 10:15:53 +0100

    Amelioration des fonctionnalites

--- Objets/Fonctionnalites/Pressing/Creation/Package/PA_Creation_Pressing.sql
+++ Objets/Fonctionnalites/Pressing/Creation/Package/PA_Creation_Pressing.sql
@@ -1,27 +1,31 @@
-CREATE OR REPLACE PACKAGE PA_PRESSING AS
-    PROCEDURE Add_pressing
+CREATE OR REPLACE PACKAGE PA_PRESSING AS     --Entete du package de creation d'un pressing
+    PROCEDURE Add_pressing           --Appel de la procedure associee
     (
+        ID_PRESS PRESSING.id%TYPE,
         NOM_PRESS PRESSING.nom%TYPE,
         DateCreation_PRESS PRESSING.date_creation%TYPE,
         IdProprietaire_PRESS PRESSING.id_proprietaire%TYPE);
-    FUNCTION Verif_nom_pres 
+    FUNCTION Verif_nom_pressing   --Appel de la fonction associee de verification de l'exixtance du pressing
     (NomPressing PRESSING.nom%TYPE) RETURN VARCHAR2;   
 END PA_PRESSING;
 /  
-CREATE OR REPLACE PACKAGE BODY PA_PRESSING AS 
+CREATE OR REPLACE PACKAGE BODY PA_PRESSING AS     --corps du package
     PROCEDURE Add_pressing
     (
+        ID_PRESS PRESSING.id%TYPE,
         NOM_PRESS PRESSING.nom%TYPE,
         DateCreation_PRESS PRESSING.date_creation%TYPE,
         IdProprietaire_PRESS PRESSING.id_proprietaire%TYPE
     )       
     IS 
-        ID_PRESS PRESSING.id%TYPE := 'PR'||seq_pressing.NEXTVAL;
-        NOM_PRESS PRESSING.nom%TYPE := '&Nom_du_pressing';
-        DateCreation_PRESS PRESSING.date_creation%TYPE := SYSDATE;
-        IdProprietaire_PRESS PRESSING.id_proprietaire%TYPE := 'UT'||seq_utilisateur.NEXTVAL;
     BEGIN
-    INSERT INTO PRESSING (id, nom, date_creation, id_proprietaire)
+    INSERT INTO PRESSING 
+        (
+            id, 
+            nom, 
+            date_creation, 
+            id_proprietaire
+        )
     VALUES
         (
             ID_PRESS, 
@@ -31,15 +35,14 @@ CREATE OR REPLACE PACKAGE BODY PA_PRESSING AS
         );
     END Add_pressing; 
     FUNCTION Verif_nom_pressing 
-    (NomPressing PRESSING.nom%TYPE) RETURN VARCHAR2
+    (NomPressing PRESSING.nom%TYPE) 
+    RETURN VARCHAR2
     IS
-    BEGIN
-    Curseur VARCHAR2(255);
-        CURSOR Curseur IS
+        CURSOR Curseur IS        --curseur qui parcourt la table pressing sur la colonne du nom
             SELECT nom
             FROM PRESSING
             WHERE (nom=NomPressing);
-        Valeur VARCHAR2 (255);
+        Valeur VARCHAR2(255);
         BEGIN
             OPEN Curseur;
             FETCH Curseur
@@ -54,11 +57,15 @@ CREATE OR REPLACE PACKAGE BODY PA_PRESSING AS
     END Verif_nom_pressing;   
 END PA_PRESSING;
 / 
-DECLARE
-    Rendu VARCHAR2 (255) := PA_PRESSING.Verif_nom_pressing('&NomPressing');
+DECLARE             --main ou partie de declaration du package
+Resultat VARCHAR2(255) := PA_PRESSING.Verif_nom_pressing('Valeur');
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('');
-    PA_PRESSING.Verif_nom_pressing(Rendu);
+    PA_PRESSING.Add_pressing         --declaration de la procedure avec les variables de substitution
+    (
+        'PR||seq_pressing.NEXTVAL',
+        '&Nom_du_pressing',
+        SYSDATE,
+        'UT||seq_utilisateur.NEXTVAL'
+    );
 END;
 /        
-
