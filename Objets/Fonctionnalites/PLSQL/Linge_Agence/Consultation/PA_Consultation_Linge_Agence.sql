b774ac1f96bfed19f6ab7b2180f703e4f10b4b1a
KengniJohan <kengnijordan9@gmail.com>
2023-01-26 23:36:51 +0100

    Modification du nom de l'enregistrement un_service en un_linge

--- Objets/Fonctionnalites/PLSQL/Linge_Agence/Consultation/PA_Consultation_Linge_Agence.sql
+++ Objets/Fonctionnalites/PLSQL/Linge_Agence/Consultation/PA_Consultation_Linge_Agence.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE PACKAGE BODY PA_CONSULTATION_LINGE_AGENCE AS
     PROCEDURE PO_CONSULTER_LINGE_AGENCE(id_agence AGENCE.id%TYPE) IS
     compteur PLS_INTEGER := 1;
     BEGIN
-        FOR un_service IN (
+        FOR un_linge IN (
                             SELECT  tl.intitule "intitule",
                                     cl.nom "nom",
                                     la.date_Ajout "date_a",
@@ -19,9 +19,9 @@ CREATE OR REPLACE PACKAGE BODY PA_CONSULTATION_LINGE_AGENCE AS
                             WHERE   la.id_agence = PO_CONSULTER_LINGE_AGENCE.id_agence
                         )
         LOOP 
-            IF un_service."date_r" IS NOT NULL THEN
-                DBMS_OUTPUT.PUT_LINE(compteur||' - '||un_service."intitule"||' '||un_service."nom"
-                                    ||' ajoute le '||un_service."date_a");
+            IF un_linge."date_r" IS NULL THEN
+                DBMS_OUTPUT.PUT_LINE(compteur||' - '||un_linge."intitule"||' '||un_linge."nom"
+                                    ||' ajoute le '||un_linge."date_a");
                 compteur := compteur + 1;
             END IF;
         END LOOP;
