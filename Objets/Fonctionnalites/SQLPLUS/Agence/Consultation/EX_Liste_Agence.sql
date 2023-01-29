07c0bb3e184e27ce41cecf1121d584be64f7ee22
KengniJohan <kengnijordan9@gmail.com>
2023-01-30 00:44:00 +0100

    Ajout d'une fonctionnalité livrable dans le menu principale des agences

--- Objets/Fonctionnalites/SQLPLUS/Agence/Consultation/EX_Liste_Agence.sql
+++ Objets/Fonctionnalites/SQLPLUS/Agence/Consultation/EX_Liste_Agence.sql
@@ -1,7 +1,8 @@
 EXECUTE PA_CONSULTATION_AGENCE.PO_CONSULTER_AGENCE('&&id_pressing')
 PROMPT              1 - la liste des agents
 PROMPT              2 - la liste des services d'une agence
-PROMPT              3 - la liste des annonces d'un agence
+PROMPT              3 - la liste des linges que traite une agence
+PROMPT              4 - la liste des annonces d'un agence
 PROMPT              A - Retour
 ACCEPT mon_choix CHAR PROMPT 'Veuillez entrer le caractere correspondant a votre choix : '
 SET TERM OFF
@@ -9,8 +10,9 @@ SET SERVEROUTPUT ON
 COLUMN script NEW_VALUE v_script 
 SELECT CASE '&mon_choix'
             WHEN '1' THEN 'Objets/Fonctionnalites/SQLPLUS/Agent/Consultation/EX_Liste_Agents'
-            WHEN '2' THEN ''
-            WHEN '3' THEN ''
+            WHEN '2' THEN 'Objets/Fonctionnalites/SQLPLUS/Service_Agence/Consultation/EX_Liste_Service_Agence'
+            WHEN '3' THEN '' 
+            WHEN '4' THEN ''
             WHEN 'A' THEN 'Objets/Fonctionnalites/SQLPLUS/Pressing/Consultation/Menu'
             ELSE 'Objets/Fonctionnalites/SQLPLUS/Agence/Consultation/ERR_Choix'
         END script
