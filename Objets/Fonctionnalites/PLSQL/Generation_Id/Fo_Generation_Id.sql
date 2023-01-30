a72991840ea198dd9daf34d0a197d6332f14561c
Mouofo Kayou Danela <mouofodanela@gmail.com>
2023-01-30 19:20:54 +0100

    modification de la procedure

--- Objets/Fonctionnalites/PLSQL/Generation_Id/Fo_Generation_Id.sql
+++ Objets/Fonctionnalites/PLSQL/Generation_Id/Fo_Generation_Id.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION Generate_id(sequence_table INT,prefixe_initial varcha
     ID_FINAL UTILISATEUR.id%type;     
     BEGIN
        IF sequence_table>=1 AND sequence_table<10 THEN
-          ID_FINAL:=prefixe_initial||'0'||sequence_table;
+          ID_FINAL:=prefixe_initial||'00'||sequence_table;
        elsif sequence_table>=10 AND sequence_table<100 THEN
          ID_FINAL:=prefixe_initial||'0'||sequence_table;
        ELSE
