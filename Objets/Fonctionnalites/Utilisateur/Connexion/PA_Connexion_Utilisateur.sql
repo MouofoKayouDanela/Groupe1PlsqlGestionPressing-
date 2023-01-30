b498a6e1be022ffb7fc008ae9e6c62bc811c5a91
bavoua <bavouamarcborel@gmail.com>
2023-01-30 21:05:48 +0100

    recuperation de l'id du pressing

--- Objets/Fonctionnalites/Utilisateur/Connexion/PA_Connexion_Utilisateur.sql
+++ Objets/Fonctionnalites/Utilisateur/Connexion/PA_Connexion_Utilisateur.sql
@@ -10,7 +10,7 @@ END PA_CONNEXION_UTILISATEUR ;
 CREATE OR REPLACE PACKAGE BODY PA_CONNEXION_UTILISATEUR AS
     FUNCTION Verif_utilisateur(
       nom_utilisateur UTILISATEUR.Nom_utilisateur%TYPE,
-      mot_de_passe UTILISATEUR.Mot_de_passe%TYPE
+      mot_de_passe UTILISATEUR.Mot_de_passe%TYPEI
     ) RETURN VARCHAR2 
     IS
     CURSOR CUR IS
