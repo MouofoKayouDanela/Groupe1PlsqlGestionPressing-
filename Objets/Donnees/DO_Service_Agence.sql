422a3202f171b11b5b874e9d157ea65f154bfc6b
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2023-02-01 11:04:16 +0100

    suite des modifications

--- Objets/Donnees/DO_Service_Agence.sql
+++ Objets/Donnees/DO_Service_Agence.sql
@@ -3,104 +3,92 @@ INSERT INTO SERVICE_AGENCE
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout   
 )
 VALUES
 (
     'AG001',
     'CS001',
     'TS002',
-    TO_DATE('17/01/2019', 'dd/mm/yyyy'),
-    TO_DATE('20/01/2019', 'dd/mm/yyyy')
+    TO_DATE('17/01/2019', 'dd/mm/yyyy')
+   
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout   
 )
 VALUES
 (
     'AG001',
      'CS003',
     'TS002',
-    TO_DATE('19/01/2019', 'dd/mm/yyyy'),
-    TO_DATE('22/01/2019', 'dd/mm/yyyy')
+    TO_DATE('19/01/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
     'AG001',
      'CS006',
     'TS002',
-    TO_DATE('19/01/2019', 'dd/mm/yyyy'),
-    TO_DATE('22/01/2019', 'dd/mm/yyyy')
+    TO_DATE('19/01/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout   
 )
 VALUES
 (
     'AG002',
     'CS007',
     'TS002',
-    TO_DATE('09/11/2019', 'dd/mm/yyyy'),
-    TO_DATE('02/12/2019', 'dd/mm/yyyy')
+    TO_DATE('09/11/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
     'AG002',
     'CS003',
     'TS002',
-    TO_DATE('19/12/2019', 'dd/mm/yyyy'),
-    TO_DATE('22/12/2019', 'dd/mm/yyyy')
+    TO_DATE('19/12/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout 
 )
 VALUES
 (
     'AG002',
     'CS004',
     'TS001',
-    TO_DATE('20/12/2019', 'dd/mm/yyyy'),
-    TO_DATE('30/12/2019', 'dd/mm/yyyy')
+    TO_DATE('20/12/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
@@ -123,87 +111,90 @@ VALUES
     'AG003',
    'CS001',
     'TS001',
-    TO_DATE('03/12/2019', 'dd/mm/yyyy'),
-    TO_DATE('10/12/2019', 'dd/mm/yyyy')
+    TO_DATE('03/12/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
     'AG003',
     'CS003',
     'TS003',
-    TO_DATE('09/12/2019', 'dd/mm/yyyy'),
-    TO_DATE('15/12/2019', 'dd/mm/yyyy')
+    TO_DATE('09/12/2019', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout 
 )
 VALUES
 (
     'AG004',
     'CS008',
     'TS004',
-   TO_DATE('26/04/2020', 'dd/mm/yyyy'),
-    TO_DATE('29/04/2020', 'dd/mm/yyyy')
+   TO_DATE('26/04/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout
 )
 VALUES
 (
     'AG004',
     'CS003',
     'TS004',
-    TO_DATE('28/04/2020', 'dd/mm/yyyy'),
-    TO_DATE('30/04/2020', 'dd/mm/yyyy')
+    TO_DATE('28/04/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
     'AG004',
     'CS008',
     'TS005',
-   TO_DATE('28/04/2020', 'dd/mm/yyyy'),
-    TO_DATE('30/04/2020', 'dd/mm/yyyy')
+   TO_DATE('28/04/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout
 )
 VALUES
 (
     'AG005',
     'CS003',
     'TS005',
-    TO_DATE('30/04/2020', 'dd/mm/yyyy'),
+    TO_DATE('30/04/2020', 'dd/mm/yyyy')
+);
+INSERT INTO SERVICE_AGENCE
+(
+    id_agence ,
+    id_categorie ,
+    id_type ,
+    Date_ajout  
+)
+VALUES
+(
+    'AG005',
+   'CS003',
+    'TS006',
     TO_DATE('02/05/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
@@ -211,94 +202,69 @@ INSERT INTO SERVICE_AGENCE
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
-)
-VALUES
-(
-    'AG005',
-   'CS003',
-    'TS006',
-    TO_DATE('02/05/2020', 'dd/mm/yyyy'),
-    TO_DATE('12/05/2020', 'dd/mm/yyyy')
-);
-INSERT INTO SERVICE_AGENCE
-(
-    id_agence ,
-    id_categorie ,
-    id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout   
 )
 VALUES
 (
     'AG005',
     'CS003',
     'TS007',
-    TO_DATE('05/05/2020', 'dd/mm/yyyy'),
-    TO_DATE('18/05/2020', 'dd/mm/yyyy')
+    TO_DATE('05/05/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout 
 )
 VALUES
 (
     'AG005',
     'CS008',
     'TS008',
-    TO_DATE('09/05/2020', 'dd/mm/yyyy'),
-    TO_DATE('20/05/2020', 'dd/mm/yyyy')
+    TO_DATE('09/05/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout  
 )
 VALUES
 (
     'AG006',
     'CS009',
     'TS009',
-    TO_DATE('31/08/2020', 'dd/mm/yyyy'),
-    TO_DATE('12/09/2020', 'dd/mm/yyyy')
+    TO_DATE('31/08/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout
 )
 VALUES
 (
     'AG006',
      'CS003',
     'TS009',
-    TO_DATE('01/09/2020', 'dd/mm/yyyy'),
-    TO_DATE('10/09/2020', 'dd/mm/yyyy')
+    TO_DATE('01/09/2020', 'dd/mm/yyyy')
 );
 INSERT INTO SERVICE_AGENCE
 (
     id_agence ,
     id_categorie ,
     id_type ,
-    Date_ajout ,
-    Date_retrait  
+    Date_ajout 
 )
 VALUES
 (  
     'AG006',
     'CS003',
     'TS010',
-    TO_DATE('05/09/2020', 'dd/mm/yyyy'),
-    TO_DATE('19/09/2020', 'dd/mm/yyyy')
+    TO_DATE('05/09/2020', 'dd/mm/yyyy')
+
 );
--- Objets/Fonctionnalites/PLSQL/Agence/Suppression/Package/PA_Service_Linge_Agence.sql
+++ Objets/Fonctionnalites/PLSQL/Agence/Suppression/Package/PA_Service_Linge_Agence.sql
@@ -2,9 +2,9 @@ SET serveroutput on;
 CREATE OR REPLACE PACKAGE PA_SERVICE_LINGE_AGENCES AS
     PROCEDURE SUPPRESSION_SERVICE_AGENCES (
             Nom_Pressing Pressing.Nom%TYPE, 
-            Lieu QUARTIER.nom%TYPE,       
-            Nom_TypeService Type_Service.Intitule%TYPE,
-            Nom_CatService Categorie_Service.nom%TYPE,
+            Lieu QUARTIER.nom%TYPE,      
+            Nom_CatService Categorie_Service.nom%TYPE, 
+            Nom_TypeService Type_Service.Intitule%TYPE, 
             date_aj SERVICE_AGENCE.date_Ajout%TYPE
     );
     PROCEDURE SUPPRESSION_LINGE_AGENCES(
@@ -18,21 +18,25 @@ END PA_SERVICE_LINGE_AGENCES;
 /
 CREATE OR REPLACE PACKAGE BODY PA_SERVICE_LINGE_AGENCES AS
    PROCEDURE SUPPRESSION_SERVICE_AGENCES (  
-            Nom_Pressing Pressing.Nom%TYPE, 
-            Lieu QUARTIER.nom%TYPE,     
-            Nom_TypeService Type_Service.Intitule%TYPE,
-            Nom_CatService Categorie_Service.nom%TYPE,
+             Nom_Pressing Pressing.Nom%TYPE, 
+            Lieu QUARTIER.nom%TYPE,      
+            Nom_CatService Categorie_Service.nom%TYPE, 
+            Nom_TypeService Type_Service.Intitule%TYPE, 
             date_aj SERVICE_AGENCE.date_Ajout%TYPE
     )IS
+    idAG varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_AGENCE(Nom_Pressing,Lieu);
     idCS varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_CATEGORIE_SERVICE(Nom_CatService);
     idTS varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_TYPE_SERVICE(Nom_TypeService);
-    idAG varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_AGENCE(Nom_Pressing,Lieu);
+    
         BEGIN
+        DBMS_OUTPUT.PUT_LINE(idAG);
+        DBMS_OUTPUT.PUT_LINE(idCS);
+        DBMS_OUTPUT.PUT_LINE(idTS);
             UPDATE SERVICE_AGENCE
             SET SERVICE_AGENCE.date_retrait=TO_DATE(Current_date,'dd/mm/yyyy')
-            WHERE SERVICE_AGENCE.id_type=idTS
-            AND SERVICE_AGENCE.id_categorie=idCS
-            AND SERVICE_AGENCE.id_agence=idAG
+            WHERE SERVICE_AGENCE.id_agence=SUPPRESSION_SERVICE_AGENCES.idAG 
+            AND SERVICE_AGENCE.id_categorie=SUPPRESSION_SERVICE_AGENCES.idCS
+            AND SERVICE_AGENCE.id_type=SUPPRESSION_SERVICE_AGENCES.idTS
             AND SERVICE_AGENCE.date_Ajout=TO_DATE(date_aj,'dd/mm/yyyy');
         END SUPPRESSION_SERVICE_AGENCES;
     
@@ -43,15 +47,19 @@ CREATE OR REPLACE PACKAGE BODY PA_SERVICE_LINGE_AGENCES AS
             Nom_TypeLinge TYPE_LINGE.Intitule%TYPE,
             date_aj LINGE_AGENCE.date_Ajout%TYPE
     )IS
+    idAG varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_AGENCE(Nom_Pressing,Lieu);
     idCL varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_CATEGORIE_LINGE (Nom_CatLinge);
     idTL varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_TYPE_LINGE (Nom_TypeLinge);
-    idAG varchar2(50):=PA_AGENCES_Nom.RECUPERATION_ID_AGENCE(Nom_Pressing,Lieu);
         BEGIN
+        DBMS_OUTPUT.PUT_LINE(idAG);
+        DBMS_OUTPUT.PUT_LINE(idCL);
+        DBMS_OUTPUT.PUT_LINE(idTL);
+
             UPDATE LINGE_AGENCE
             SET LINGE_AGENCE.date_retrait=TO_DATE(Current_date,'dd/mm/yyyy')
-            WHERE LINGE_AGENCE.id_type=idTL
+            WHERE LINGE_AGENCE.id_agence=idAG
             AND LINGE_AGENCE.id_categorie=idCL
-            AND LINGE_AGENCE.id_agence=idAG
+            AND LINGE_AGENCE.id_type=idTL
             AND LINGE_AGENCE.date_Ajout=TO_DATE(date_aj,'dd/mm/yyyy');
         END SUPPRESSION_LINGE_AGENCES;
 END PA_SERVICE_LINGE_AGENCES;
